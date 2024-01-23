ee1dc8201bdb1ee6d55bf05908f9d76b2ad701b0
Michio Katouda <katouda@rist.or.jp>
2024-01-23 19:43:34 +0900

    Improve to treat variouns input coordinate formats (ex.: pdb, pdbqt, mol2 etc.) with openbabel

--- pystrainfilter/pystrainfilter.py
+++ pystrainfilter/pystrainfilter.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-import sys
 import os
-import shutil
 import argparse
 import subprocess
 
@@ -19,17 +17,17 @@ def get_parser():
     parser = argparse.ArgumentParser(
         description="StrainFilter system call interface"
     )
+    #parser.add_argument(
+    #    "-i","--input",
+    #    help="yaml style input file",
+    #    type=str,
+    #    default='input.yml'
+    #)
     parser.add_argument(
-        "-i","--input",
-        help="yaml style input file",
+        "-c","--coord",
+        help="input coordinate file: file format can be treated with openbabel",
         type=str,
-        default='input.yml'
-    )
-    parser.add_argument(
-        "-s","--sdf",
-        help="input sdf file",
-        type=str,
-        dest='sdf_path',
+        dest='coord_path',
         default='input.sdf'
     )
     parser.add_argument(
@@ -77,18 +75,19 @@ def set_config(args):
     return conf
 
 
-def run_strainfilter(sdf_path, script_path, emax_total_strain=6.5,
+def run_strainfilter(coord_path, script_path, emax_total_strain=6.5,
                      emax_torsion=1.8, verbose=False, **kwargs):
 
-    basename = os.path.splitext(os.path.basename(sdf_path))[0]
+    [basename, ext] = os.path.splitext(os.path.basename(coord_path))
+    ext = ext.lower()
     mol2_path = basename + '.mol2'
-    #ob_mols = pybel.readfile('sdf', sdf_path)
+    #ob_mols = pybel.readfile(ext[1:], coord_path)
     #mol2file = pybel.Outputfile('mol2', mol2_path, overwrite=True)
     #for ob_mol in ob_mols:
     #    mol2file.write(ob_mol)
     #mol2file.close()
     cmd = [
-        'obabel', '-isdf', sdf_path, '-omol2', '-O', mol2_path, '-xu'
+        'obabel', '-i'+ext[1:], coord_path, '-omol2', '-O', mol2_path, '-xu'
     ]
     print(' '.join(cmd), flush=True)
     results = subprocess.run(
@@ -100,9 +99,7 @@ def run_strainfilter(sdf_path, script_path, emax_total_strain=6.5,
     os.chdir(script_path)
 
     cmd = [
-        'python',
-        'Torsion_Strain.py',
-        cw_dir + '/' + mol2_path
+        'python', 'Torsion_Strain.py', cw_dir + '/' + mol2_path
     ]
     print(' '.join(cmd), flush=True)
 
@@ -117,15 +114,36 @@ def run_strainfilter(sdf_path, script_path, emax_total_strain=6.5,
     df = pd.read_csv(csv_path, header=None)[[1, 5]]
     print('csv_path:', csv_path, '\n', df, flush=True)
     df_filtered = df[(df[1] <= emax_total_strain) & (df[5] <= emax_torsion)]
-    print(df_filtered, flush=True)
+    csvout_path = basename + '_strain_filtered.csv'
+    df_filtered.to_csv(csvout_path, header=None)
+    print('strain filtered scores:\n', df_filtered, flush=True)
     idx_filtered = df_filtered.index.to_list()
-    print('idx_filtered:', idx_filtered, flush=True)
+    if verbose: print('idx_filtered:', idx_filtered, flush=True)
 
-    sdfout_path = basename + '_strain_filtered.sdf' 
-    rd_mols = list(Chem.SDMolSupplier(sdf_path, removeHs=False))
-    with Chem.SDWriter(sdfout_path) as writer:
-        for i in idx_filtered:
-            writer.write(rd_mols[i])
+    if len(idx_filtered) > 0:
+
+        if not ext == '.sdf':
+            sdf_path = basename + '.sdf'
+            cmd = [
+                'obabel', '-i'+ext[1:], coord_path, '-osdf', '-O', sdf_path, '-xu'
+            ]
+            print(' '.join(cmd), flush=True)
+            results = subprocess.run(
+                cmd, capture_output=True, check=True, text=True
+            )
+            if verbose: print(results.stdout, results.stderr, flush=True)
+        else:
+            sdf_path = coord_path
+        
+        sdfout_path = basename + '_strain_filtered.sdf'
+        rd_mols = list(Chem.SDMolSupplier(sdf_path, removeHs=False))
+        with Chem.SDWriter(sdfout_path) as writer:
+            for i in idx_filtered:
+                rd_mol = rd_mols[i]
+                if rd_mol is not None:
+                    rd_mol.SetProp('total_strain_energy', str(df.iloc[i,0]))
+                    rd_mol.SetProp('dihedral_torsion_strain_energy', str(df.iloc[i,1]))
+                    writer.write(rd_mol)
 
     return idx_filtered
 
@@ -141,7 +159,7 @@ def main():
         print('{}: {}'.format(k, v), flush=True)
     print('====================================', flush=True)
 
-    if os.path.isdir(conf['script_path']):
+    if os.path.isdir(conf['script_path']) and os.path.exists(conf['coord_path']):
         run_strainfilter(**conf)
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pystrainfilter",
-    version="0.2.1",
+    version="0.1",
     entry_points={
         "console_scripts": [
             "pystrainfilter=pystrainfilter.pystrainfilter:main",
